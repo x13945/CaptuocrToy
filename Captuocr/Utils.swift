a738fb7f30e187ed4335fd177d4b4851dd0d1300
shaw L <lsk.boy.f@gmail.com>
2023-05-11 22:20:49 +0800

    适配m1芯片

--- Captuocr/Utils.swift
+++ Captuocr/Utils.swift
@@ -7,9 +7,34 @@
 //
 
 import Foundation
+
+import Cocoa
+
+
 class Utils {
+    static func hasScreenCapturePermission() -> Bool {
+        let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true]
+        return AXIsProcessTrustedWithOptions(options as CFDictionary)
+    }
+    
     static func capturePic() -> String? {
-        let destinationPath = "/tmp/\(UUID().uuidString).png"
+        //        if !hasScreenCapturePermission() {
+        //            let alert = NSAlert()
+        //            alert.messageText = "Screenshot permission needed"
+        //            alert.informativeText = "This app needs permission to take screenshots. Please grant the permission in the System Preferences."
+        //            alert.addButton(withTitle: "Open System Preferences")
+        //            alert.addButton(withTitle: "Cancel")
+        //
+        //            let response = alert.runModal()
+        //            if response == NSApplication.ModalResponse.alertFirstButtonReturn {
+        //                if let url = URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy_ScreenCapture") {
+        //                    NSWorkspace.shared.open(url)
+        //                }
+        //            }
+        //            return nil
+        //        } else {
+        // 权限已授权，可以开始录屏
+        let destinationPath = NSTemporaryDirectory() + UUID().uuidString + ".png"
         let task = Process()
         task.launchPath = "/usr/sbin/screencapture"
         task.arguments = ["-i", "-r", destinationPath]
@@ -17,7 +42,10 @@ class Utils {
         task.waitUntilExit()
         var notDir = ObjCBool(false)
         return FileManager.default.fileExists(atPath: destinationPath, isDirectory: &notDir)
-            ? destinationPath
-            : nil
+        ? destinationPath
+        : nil
+        
+        //        }
+        
     }
 }
